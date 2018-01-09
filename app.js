42301be0ff23da86ee537a4b0134ff6ebabfcf8f
Jhon Gil Sepulveda <jhongil96@gmail.com>
2018-01-08 21:09:51 -0500

    Greetings

--- app.js
+++ app.js
@@ -20,3 +20,59 @@ app.get("/webhook", (req, res) => {
         res.sendStatus(403);
     }
 });
+
+app.post("/webhook", (req, res) => {
+    if(req.body.object == "page") {
+        req.body.entry.forEach(entry => {
+            entry.messaging.forEach(event => {
+                if(event.postback) {
+                    processPostback(event);
+                }
+            })
+        });
+
+        res.sendStatus(200);
+    }
+})
+
+function processPostback(event) {
+    var senderId = event.sender.id;
+    var payload = event.postback.payload;
+
+    if(payload === "Greeting") {
+        request({
+            url: "https://graph.facebook.com/v2.6/" + senderId,
+            qs: {
+                access_token = process.env.PAGE_ACCESS_TOKEN,
+                fields: "first_name"
+            },
+            method: "GET"
+        }, (error, response, body) => {
+            var greeting = "";
+
+            if(error) {
+                console.log("Error getting user's name: " + error);
+            } else {
+                var bodyObj = JSON.parse(body);
+                name = bodyObj.first_name;
+                greeting = "Hi " + name + ". ";
+            }
+            var message = greeting + "My name is RanKey bot. Do you want to see some memes?";
+            sendMessage(senderId, {text: message});
+        });
+    }
+}
+
+function sendMessage(recipientId, message) {
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: "POST",
+        json: {
+            recipientId: {id: recipientId},
+            message: message,
+        }
+    }, (error, response, body) => {
+        console.log("Error sending message :" + error);
+    })
+}
